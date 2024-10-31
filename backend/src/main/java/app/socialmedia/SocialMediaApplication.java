ddf235fe2d07ac420776b82a068e6d23efb61464
geoorgeqq <grajdeanu.george@yahoo.ro>
2024-11-01 00:15:47 +0200

    test credentials

--- backend/src/main/java/app/socialmedia/SocialMediaApplication.java
+++ backend/src/main/java/app/socialmedia/SocialMediaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialMediaApplication {
     public static void main(String[] args) {
+        System.out.println(" ");
         SpringApplication.run(SocialMediaApplication.class, args);
     }
 
