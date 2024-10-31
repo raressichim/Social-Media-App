708774badf6344020c1ec4d8b9e5e3e55d9823de
geoorgeq <grajdeanu.george@yyahoo.ro>
2024-10-31 23:49:50 +0200

    test2

--- backend/src/main/java/app/socialmedia/SocialMediaApplication.java
+++ backend/src/main/java/app/socialmedia/SocialMediaApplication.java
@@ -8,6 +8,7 @@ public class SocialMediaApplication {
     public static void main(String[] args) {
         System.out.println("test merge");
         System.out.println("GEORGE");
+        System.out.println(" ");
         SpringApplication.run(SocialMediaApplication.class, args);
     }
 
