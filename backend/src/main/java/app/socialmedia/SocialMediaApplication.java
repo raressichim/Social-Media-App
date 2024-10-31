32b59d7612f0855cf406558191977cda75f829dc
Rares <rares@Raress-MacBook-Pro.local>
2024-10-31 22:52:11 +0200

    test

--- backend/src/main/java/app/socialmedia/SocialMediaApplication.java
+++ backend/src/main/java/app/socialmedia/SocialMediaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialMediaApplication {
     public static void main(String[] args) {
+        System.out.println("test for git");
         SpringApplication.run(SocialMediaApplication.class, args);
     }
 
