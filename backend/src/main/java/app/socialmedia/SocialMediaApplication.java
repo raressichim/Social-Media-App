03a9d8098b909b1950b3495eb7af76c732a25926
raressichim <raressic@gmail.com>
2024-11-01 00:09:18 +0200

    remove souts

--- backend/src/main/java/app/socialmedia/SocialMediaApplication.java
+++ backend/src/main/java/app/socialmedia/SocialMediaApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialMediaApplication {
     public static void main(String[] args) {
-        System.out.println("test merge");
-        System.out.println("GEORGE");
-        System.out.println(" ");
         SpringApplication.run(SocialMediaApplication.class, args);
     }
 
