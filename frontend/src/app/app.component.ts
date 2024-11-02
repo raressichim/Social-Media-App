1bf715f03dd428960f7aefc1df23cd72c7219375
geoorgeqq <grajdeanu.george@yahoo.ro>
2024-11-02 13:53:39 +0200

    Implement login form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
